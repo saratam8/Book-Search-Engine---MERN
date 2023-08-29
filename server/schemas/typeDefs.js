f8953b54792d5ed91168ada972b1dc261e66bd4f
Potatonaut <stam296@gmail.com>
2023-08-28 21:26:02 -0500

    Updated server.js to use apollo server. Added schemas folder which contains the resolvers and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    me: [Tech]
+  }
+
+  type Mutation {
+    login(user: {username: String, email: String, password: String}): Auth
+    addUser(user: {username: String, email: String, password: String}): Auth
+    saveBooks(Book: {bookId: String, authors: [String], description: String, title: String, image: String, link: String}): Book
+    removeBook(bookId: String): User
+  }
+`;
+
+module.exports = typeDefs;
